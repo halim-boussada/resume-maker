a384b15f7e656b5c764df80efa2104b61be5cce4
halim-boussada <halimboussada10@gmail.com>
2021-03-22 20:05:33 +0100

    first day

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
 @Component({
   selector: "app-resume",
   templateUrl: "./resume.component.html",
@@ -20,5 +21,20 @@ export class ResumeComponent implements OnInit {
     //     this.data = data;
     //   });
     this.data = JSON.parse(localStorage.getItem("cv"));
+  }
+  captureScreen() {
+    const data = document.getElementById("contentToConvert");
+    html2canvas(data).then((canvas) => {
+      // Few necessary setting options
+      const imgWidth = 200;
+      const pageHeight = 500;
+      const imgHeight = (canvas.height * 300) / canvas.width;
+      const heightLeft = imgHeight;
+      const contentDataURL = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4"); // A4 size page of PDF
+      const position = 0;
+      pdf.addImage(contentDataURL, "PNG", 25, position, imgWidth, imgHeight);
+      pdf.save("testPdf.pdf"); // Generated PDF
+    });
   }
 }
