feb69380a36667504594f35be3e7633aef825303
halim-boussada <halimboussada10@gmail.com>
2021-04-30 14:19:43 +0100

    design ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,78 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { FormsModule } from "@angular/forms";
-import { ResumeComponent } from './resume/resume.component';
-import { CoachesListComponent } from './coaches-list/coaches-list.component';
-import { OneCoacheComponent } from './one-coache/one-coache.component';
-import { MotivationLetterComponent } from './motivation-letter/motivation-letter.component';
-import { NotificationComponent } from './notification/notification.component';
-import { NavbarUserComponent } from './navbar-user/navbar-user.component';
-import { AdminComponent } from './admin/admin.component';
-import { AddCoachComponent } from './add-coach/add-coach.component';
-import { ManegeUsersComponent } from './manege-users/manege-users.component';
-import { ManegeCoachesComponent } from './manege-coaches/manege-coaches.component';
-import { NavbarAdminComponent } from './navbar-admin/navbar-admin.component';
-import { CoacheHomeComponent } from './coache-home/coache-home.component';
-import { RequestsListComponent } from './requests-list/requests-list.component';
-import { CheckUserProfileComponent } from './check-user-profile/check-user-profile.component';
-import { FeedbackComponent } from './feedback/feedback.component';
-import { OneMlComponent } from './one-ml/one-ml.component';
-import { RequestArchieveComponent } from './request-archieve/request-archieve.component';
-import { AddUserDataComponent } from './add-user-data/add-user-data.component';
-import { DownloadResumeComponent } from './download-resume/download-resume.component';
+import { ResumeComponent } from "./resume/resume.component";
+import { CoachesListComponent } from "./coaches-list/coaches-list.component";
+import { OneCoacheComponent } from "./one-coache/one-coache.component";
+import { MotivationLetterComponent } from "./motivation-letter/motivation-letter.component";
+import { NotificationComponent } from "./notification/notification.component";
+import { NavbarUserComponent } from "./navbar-user/navbar-user.component";
+import { AdminComponent } from "./admin/admin.component";
+import { AddCoachComponent } from "./add-coach/add-coach.component";
+import { ManegeUsersComponent } from "./manege-users/manege-users.component";
+import { ManegeCoachesComponent } from "./manege-coaches/manege-coaches.component";
+import { NavbarAdminComponent } from "./navbar-admin/navbar-admin.component";
+import { CoacheHomeComponent } from "./coache-home/coache-home.component";
+import { RequestsListComponent } from "./requests-list/requests-list.component";
+import { CheckUserProfileComponent } from "./check-user-profile/check-user-profile.component";
+import { FeedbackComponent } from "./feedback/feedback.component";
+import { OneMlComponent } from "./one-ml/one-ml.component";
+import { RequestArchieveComponent } from "./request-archieve/request-archieve.component";
+import { AddUserDataComponent } from "./add-user-data/add-user-data.component";
+import { DownloadResumeComponent } from "./download-resume/download-resume.component";
+//////
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { HomeComponent } from "./home/home.component";
+import { MatBadgeModule } from "@angular/material/badge";
+import { from } from "rxjs";
+import { MatSliderModule } from "@angular/material/slider";
 
 @NgModule({
-  declarations: [AppComponent, ResumeComponent, CoachesListComponent, OneCoacheComponent, MotivationLetterComponent, NotificationComponent, NavbarUserComponent, AdminComponent, AddCoachComponent, ManegeUsersComponent, ManegeCoachesComponent, NavbarAdminComponent, CoacheHomeComponent, RequestsListComponent, CheckUserProfileComponent, FeedbackComponent, OneMlComponent, RequestArchieveComponent, AddUserDataComponent, DownloadResumeComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    ResumeComponent,
+    CoachesListComponent,
+    OneCoacheComponent,
+    MotivationLetterComponent,
+    NotificationComponent,
+    NavbarUserComponent,
+    AdminComponent,
+    AddCoachComponent,
+    ManegeUsersComponent,
+    ManegeCoachesComponent,
+    NavbarAdminComponent,
+    CoacheHomeComponent,
+    RequestsListComponent,
+    CheckUserProfileComponent,
+    FeedbackComponent,
+    OneMlComponent,
+    RequestArchieveComponent,
+    AddUserDataComponent,
+    DownloadResumeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatBadgeModule,
+    FlexLayoutModule,
+    MatSliderModule,
+  ],
 
   providers: [],
   bootstrap: [AppComponent],
